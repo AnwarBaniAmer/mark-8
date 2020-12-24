9ae55676d8df45b49b7c2663d256bb30d977da30
Anwar Bani Amer <anwar.baniamer@roya.tv.com>
2020-12-24 15:52:35 +0200

    mark-8

--- app/src/main/java/com/mark8/view/SplashActivity.java
+++ app/src/main/java/com/mark8/view/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        startActivity(new Intent(this, WelcomeActivity.class));
+        startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
         finish();
     }
 }
